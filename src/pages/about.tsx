f572361e4db0655974633684394b3edfafd78562
nishidayoshikatsu <nsd24technology@gmail.com>
2021-11-09 07:35:22 +0900

    :hammer: 各種ページのベースを作成

---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const About: NextPage = () => {
+  return <>説明ページ</>;
+};
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Top: NextPage = () => {
-  return <></>;
+  return <>トップページ</>;
 };
 
 export default Top;
