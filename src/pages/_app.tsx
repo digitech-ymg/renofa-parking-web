d5e371a7b39d5e0e4185d3eab8a59455c9be176d
nishidayoshikatsu <nsd24technology@gmail.com>
2021-11-09 07:32:29 +0900

    :fire: 利用しないファイルを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "@/styles/globals.css";
-import Header from "@/components/Header";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+
+import Header from "@/components/Header";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
