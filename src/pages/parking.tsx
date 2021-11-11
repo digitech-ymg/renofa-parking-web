ad74dbace5d7d92b5f6bb60af20be5b800305dea
Wataru Ono <wtr0414@gmail.com>
2021-11-11 19:53:05 +0900

    ✨ 駐車場に関する Select を追加
    駐車場を選べるようにするため

--- src/pages/parking.tsx
+++ src/pages/parking.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from "next";
+import { Select } from "@chakra-ui/select";
+import { Container } from "@chakra-ui/layout";
+
+type Parking = {
+  name: string;
+};
+
+const parkings: Parking[] = [
+  { name: "有料駐車場" },
+  { name: "JA山口" },
+  { name: "トラック協会" },
+  { name: "河川敷" },
+  { name: "パナソニック" },
+];
 
 const Parking: NextPage = () => {
-  return <>駐車場ページ</>;
+  return (
+    <Container h="100vh" bgColor="white">
+      <Select color="gray.700" borderColor="gray.200">
+        {parkings.map((parking) => (
+          <option key={parking.name}>{parking.name}</option>
+        ))}
+      </Select>
+    </Container>
+  );
 };
 
 export default Parking;
