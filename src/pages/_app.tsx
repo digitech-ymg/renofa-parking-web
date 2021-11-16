3e4aae3ff3c7ad3c7b9fa14c4c350cc293bdaffb
tetsuyanh <tetsuyanh@gmail.com>
2021-11-16 23:22:29 +0900

    📈 GA計測の追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,46 @@
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import * as gtag from "../lib/gtag";
 
 import Header from "@/components/Header";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handlerRouteChange = (path: string) => {
+      gtag.pageview(path);
+    };
+    router.events.on("routeChangeComplete", handlerRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handlerRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider>
-      <Header />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script
+        defer
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" defer strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtag.GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <ChakraProvider>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 
