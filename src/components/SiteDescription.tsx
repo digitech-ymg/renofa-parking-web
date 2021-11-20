0c9c8c30a8a080e535d89db8ecbe6982d2977601
nishidayoshikatsu <nsd24technology@gmail.com>
2021-11-21 00:34:05 +0900

    :sparkles: このサイトについてのコンポーネントを作成

---
+++ src/components/SiteDescription.tsx
@@ -0,0 +1,19 @@
+import { Center, Square, Circle, Heading, Text, Box } from "@chakra-ui/react";
+import { InfoIcon } from "@chakra-ui/icons";
+import type { VFC } from "react";
+
+const SiteDescription: VFC = () => {
+  return (
+    <Center bg="white" h="72px" color="black" border="1px" borderColor="gray.200">
+      <InfoIcon w={8} h={8} color="gray.500" />
+      <Box ml="3">
+        <Heading as="h5" size="sm">
+          このサイトについて
+        </Heading>
+        <Text fontSize="sm">はじめての方は必ずお読みください</Text>
+      </Box>
+    </Center>
+  );
+};
+
+export default SiteDescription;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { Center, Square, Circle } from "@chakra-ui/react";
+import { Container, Box, Link } from "@chakra-ui/react";
+import SiteDescription from "@/components/SiteDescription";
 
 const Top: NextPage = () => {
-  return <>トップページ</>;
+  return (
+    <Container h="100vh" bgColor="white">
+      <Box pt={5} mb={5}>
+        <Link href="/about">
+          <SiteDescription />
+        </Link>
+      </Box>
+    </Container>
+  );
 };
 
 export default Top;
