436de1d926fe94e776ca9a4e544fe9d06c4bef44
nishidayoshikatsu <nsd24technology@gmail.com>
2021-11-21 03:21:02 +0900

    :lipstick: 外部リンクにアイコンを追加

--- src/components/RenofaBanner.tsx
+++ src/components/RenofaBanner.tsx
@@ -1,11 +1,13 @@
 import type { VFC } from "react";
 import { Center, Heading } from "@chakra-ui/react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 const RenofaBanner: VFC = () => {
   return (
     <Center bg="orange.400" h="61px" color="white">
       <Heading as="h5" size="sm">
         レノファ公式サイト
+        <ExternalLinkIcon mx="1" />
       </Heading>
     </Center>
   );
