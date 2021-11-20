1f4abd72eb41705c2c8a4bb2bf6010389fae98df
nishidayoshikatsu <nsd24technology@gmail.com>
2021-11-21 02:17:52 +0900

    :sparkles: レノファのバナーを作成

---
+++ src/components/RenofaBanner.tsx
@@ -0,0 +1,14 @@
+import type { VFC } from "react";
+import { Box, Center, Heading } from "@chakra-ui/react";
+
+const RenofaBanner: VFC = () => {
+  return (
+    <Center bg="orange.400" h="61px" color="white">
+      <Heading as="h5" size="sm">
+        レノファ公式サイト
+      </Heading>
+    </Center>
+  );
+};
+
+export default RenofaBanner;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Center, Square, Circle } from "@chakra-ui/react";
 import { Container, Box, Link } from "@chakra-ui/react";
 import SiteDescription from "@/components/SiteDescription";
 import ParkingList from "@/components/ParkingList";
+import RenofaBanner from "@/components/RenofaBanner";
 
 const Top: NextPage = () => {
   return (
@@ -12,7 +13,14 @@ const Top: NextPage = () => {
           <SiteDescription />
         </Link>
       </Box>
-      <ParkingList />
+      <Box pt={5} mb={5}>
+        <ParkingList />
+      </Box>
+      <Box pt={5} mb={5}>
+        <Link href="https://www.renofa.com/" isExternal>
+          <RenofaBanner />
+        </Link>
+      </Box>
     </Container>
   );
 };
