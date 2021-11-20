74bedfbed002925d3b55497e1baa8726ee59982c
nishidayoshikatsu <nsd24technology@gmail.com>
2021-11-21 02:30:17 +0900

    :sparkles: 駐車場リストの遷移先URLにクエリパラメータを付与

--- src/components/ParkingList.tsx
+++ src/components/ParkingList.tsx
@@ -8,27 +8,41 @@ import {
   Box,
   Flex,
   Spacer,
+  Link,
+  Button,
 } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import data from "@/data/data.json";
+import { useRouter } from "next/router";
 
 const parkings = data.parkings;
 console.info(parkings);
 
 const ParkingList: VFC = () => {
+  const router = useRouter();
   return (
     <Stack
       spacing={4}
       divider={<StackDivider borderColor={useColorModeValue("gray.100", "gray.700")} />}
     >
       {parkings.map((parking) => (
-        <Flex key="parking.key">
-          <Heading as="h5" size="sm" ml="3">
-            {parking.name}
-          </Heading>
-          <Spacer />
-          <ChevronRightIcon mr="3" />
-        </Flex>
+        <Box
+          key={parking.key}
+          onClick={() => {
+            router.push({
+              pathname: "/parking", //URL
+              query: { parking: parking.key }, //検索クエリ
+            });
+          }}
+        >
+          <Flex>
+            <Heading as="h5" size="sm" ml="3">
+              {parking.name}
+            </Heading>
+            <Spacer />
+            <ChevronRightIcon mr="3" />
+          </Flex>
+        </Box>
       ))}
     </Stack>
   );
