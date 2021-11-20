88de11011d62547b533476667394c7e0467e05ac
Wataru Ono <wtr0414@gmail.com>
2021-11-21 00:41:03 +0900

    🚑 SSR の削除
    firebase hosting のビルドに失敗したため

--- src/pages/parking.tsx
+++ src/pages/parking.tsx
@@ -1,19 +1,22 @@
-import type { GetServerSidePropsContext, NextPage } from "next";
+import type { NextPage } from "next";
 import { Select } from "@chakra-ui/select";
 import { Container } from "@chakra-ui/layout";
 import data from "@/data/data.json";
 import { Box } from "@chakra-ui/react";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, useEffect } from "react";
 import { Parking } from "@/types/Parking";
 import ParkingMessage from "@/components/ParkingMessage";
+import { useRouter } from "next/router";
 
-type Props = {
-  parking: Parking;
-};
-
-const Parking: NextPage<Props> = ({ parking }) => {
-  const [selectedParking, setSelectedParking] = useState<Parking>(parking);
+const Parking: NextPage = () => {
+  const [selectedParking, setSelectedParking] = useState<Parking>();
+  const router = useRouter();
   const parkings = data.parkings;
+
+  useEffect(() => {
+    const parking = parkings.filter((parking) => parking.key === router.query.parking)[0];
+    setSelectedParking(parking);
+  }, [router.query.parking]);
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     // 渡された key から駐車場を特定する
@@ -21,38 +24,29 @@ const Parking: NextPage<Props> = ({ parking }) => {
     setSelectedParking(parking);
   };
 
-  return (
-    <Container h="100vh" bgColor="white">
-      <Box pt={5} mb={5}>
-        <Select
-          color="gray.700"
-          borderColor="gray.200"
-          defaultValue={parking.key}
-          onChange={handleChange}
-        >
-          {parkings.map((parking) => (
-            <option key={parking.key} value={parking.key}>
-              {parking.name}
-            </option>
-          ))}
-        </Select>
-      </Box>
-      <ParkingMessage parking={selectedParking} />
-    </Container>
-  );
-};
-
-export const getServerSideProps = (context: GetServerSidePropsContext) => {
-  const parkings = data.parkings;
-
-  // 渡された key から駐車場を特定する
-  const parking = parkings.filter((parking) => parking.key === context.query.parking)[0];
-
-  return {
-    props: {
-      parking: parking,
-    },
-  };
+  if (selectedParking) {
+    return (
+      <Container h="100vh" bgColor="white">
+        <Box pt={5} mb={5}>
+          <Select
+            color="gray.700"
+            borderColor="gray.200"
+            defaultValue={selectedParking.key}
+            onChange={handleChange}
+          >
+            {parkings.map((parking) => (
+              <option key={parking.key} value={parking.key}>
+                {parking.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+        <ParkingMessage parking={selectedParking} />
+      </Container>
+    );
+  } else {
+    return <>loading...</>;
+  }
 };
 
 export default Parking;
