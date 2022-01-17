49f566316734d94bb88a94f2e4fb79edaa2a672d
Masahiro Yoshidomi <abelvitz6566@gmail.com>
2022-01-17 11:21:47 +0900

    💄 実装しました

--- src/pages/parking.tsx
+++ src/pages/parking.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
-import { Select } from "@chakra-ui/select";
 import { Container } from "@chakra-ui/layout";
 import data from "@/data/data.json";
-import { Box, Stack, Image } from "@chakra-ui/react";
+import { Box, Stack, Image, Heading, Center } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption } from "@chakra-ui/react";
 import { ChangeEvent, useState, useEffect } from "react";
-import { Parking } from "@/types/Parking";
-import ParkingMessage from "@/components/ParkingMessage";
+import { Parking, ParkingInfo } from "@/types/Parking";
 import { useRouter } from "next/router";
 
 const Parking: NextPage = () => {
   const [selectedParking, setSelectedParking] = useState<Parking>();
+  const [parkingInfos, setParkingInfos] = useState<ParkingInfo[]>();
   const router = useRouter();
   const parkings = data.parkings;
 
@@ -22,34 +22,63 @@ const Parking: NextPage = () => {
     // 渡された key から駐車場を特定する
     const parking = parkings.filter((parking) => parking.key === e.target.value)[0];
     setSelectedParking(parking);
+
+    const parkingInfos = [
+      {
+        head: "正式名称",
+        content: parking.officialName,
+      },
+      {
+        head: "住所",
+        content: parking.address,
+      },
+      {
+        head: "収容台数",
+        content: `${parking.carCapacity}台`,
+      },
+      {
+        head: "スタジアムまでの距離",
+        content: `${parking.distanceToStadium}m`,
+      },
+      {
+        head: "スタジアムまでの時間",
+        content: `${parking.timeToStadium}分`,
+      },
+      {
+        head: "開場時間",
+        content: `${parking.hourToOpen}時間`,
+      },
+      {
+        head: "閉場時間",
+        content: `${parking.hourToClose}時間`,
+      },
+    ];
+    setParkingInfos(parkingInfos);
   };
 
-  if (selectedParking) {
+  if (selectedParking && parkingInfos) {
     return (
       <Box bgColor="white">
-        <Container>
-          <Select
-            color="gray.700"
-            borderColor="gray.200"
-            defaultValue={selectedParking.key}
-            onChange={handleChange}
-            py={5}
-          >
-            {parkings.map((parking) => (
-              <option key={parking.key} value={parking.key}>
-                {parking.name}
-              </option>
-            ))}
-          </Select>
-          <ParkingMessage parking={selectedParking} />
-          <Box my={5}>
-            <iframe
-              src={`https://maps.google.com/maps?output=embed&q=${selectedParking.latitude},${selectedParking.longitude}&t=m&hl=ja&z=15`}
-              width="100%"
-              height="360"
-              loading="lazy"
-            ></iframe>
-          </Box>
+        <Container h="">
+          <Center h="64px">
+            <Heading as="h1" size="sm">
+              {selectedParking.name}
+            </Heading>
+          </Center>
+          <Table variant="unstyled">
+            <Tbody>
+              {parkingInfos.map((info, index) => (
+                <Tr key={index}>
+                  <Th width="50%" backgroundColor="gray.50" fontWeight="normal" border="1px">
+                    {info.head}
+                  </Th>
+                  <Td width="50%" border="1px">
+                    {info.content}
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
           <Stack my={5}>
             {selectedParking.images.map((image, index) => (
               <Image
