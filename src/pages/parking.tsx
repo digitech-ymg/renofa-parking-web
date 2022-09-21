5f6795881d8266e89fd25749cdd43f77d132390b
YasuiYuya0 <yasui0302000@gmail.com>
2022-09-21 20:09:05 +0900

    add-reno

--- src/pages/parking.tsx
+++ src/pages/parking.tsx
@@ -93,6 +93,7 @@ const Parking: NextPage = () => {
         {parking && (
           <>
             <Center h="64px">
+              <Image width="7%" objectFit="cover" src="/reno_02.png" alt="レノ丸" />
               <Heading as="h1" size="sm">
                 {parking.name}
               </Heading>
