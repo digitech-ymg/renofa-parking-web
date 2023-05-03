ea211e77437a58c93951fb847b55319fcdff595b
tetsuyanh <tetsuyanh@gmail.com>
2023-05-03 20:20:14 +0900

    fix bug of isOffSeason

--- src/utils/game.spec.ts
+++ src/utils/game.spec.ts
@@ -42,4 +42,16 @@ describe("isOffSeason関数のテスト", () => {
     const nextGameDay = new Date("2023-01-01");
     expect(isOffSeason(today, nextGameDay)).toBe(true);
   });
+
+  it("日付が同じで試合時間の前、false", () => {
+    const today = new Date("2023-05-03T12:00:00");
+    const nextGameDay = new Date("2023-05-03T14:00:00");
+    expect(isOffSeason(today, nextGameDay)).toBe(false);
+  });
+
+  it("日付が同じで試合時間の後、false", () => {
+    const today = new Date("2023-05-03T15:00:00");
+    const nextGameDay = new Date("2023-05-03T14:00:00");
+    expect(isOffSeason(today, nextGameDay)).toBe(false);
+  });
 });
--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,5 +1,12 @@
 export const isOffSeason = (today: Date, nextGameDay: Date): boolean => {
-  const diff = nextGameDay.getTime() - today.getTime();
+  // 年月日だけを参照して、日時はそろえる
+  const todayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+  const gameDate = new Date(
+    nextGameDay.getFullYear(),
+    nextGameDay.getMonth(),
+    nextGameDay.getDate()
+  );
+  const diff = gameDate.getTime() - todayDate.getTime();
   const diffInDays = diff / (1000 * 60 * 60 * 24);
 
   return diffInDays < 0 || 30 <= diffInDays;
